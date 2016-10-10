8c9abd0476041075452cf718b497370ae1c4ac2a
hujuu <out.of.the.base.sfc@gmail.com>
2016-10-10 13:34:12 +0900

    グラフにラベルが付くように変更しました

--- 20161002_rba_4/auction.R
+++ 20161002_rba_4/auction.R
@@ -3,8 +3,14 @@ library(ggplot2) # ライブラリの読み込み
 # データの読み込み(ディレクトリは適宜、使用環境によって修正してください)
 data <- read.csv("rba/20161002_rba_4/auction.csv",header=TRUE)
 
+head(data)
+
 # 値の大きさによってプロットの点の大きさを変えた散布図
-ggplot(data, aes(x=amount, y=rate, size=amount)) +
+sp <- ggplot(data, aes(x=amount, y=rate, size=amount)) +
   geom_point(alpha=0.9) +
   scale_color_discrete() +
   coord_cartesian()
+sp
+
+# 各点にラベルを付ける
+sp + geom_text(aes(y = rate+.9, label=artist), size = 4, vjust=0)
